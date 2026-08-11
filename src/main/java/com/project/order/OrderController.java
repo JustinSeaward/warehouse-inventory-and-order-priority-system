0bec9a17bb3a6fcab19f3c4d5370483445ed3f3a
Justin Seaward <jut_seaward@yahoo.com>
2026-08-10 22:42:22 -0230

    OrderController: added endpoint mnethods to get all orders, create new orders and add order to priority tree

--- src/main/java/com/project/order/OrderController.java
+++ src/main/java/com/project/order/OrderController.java
@@ -1,4 +1,45 @@
 package com.project.order;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/order")
+@CrossOrigin
 public class OrderController {
+    @Autowired
+    private OrderService  orderService;
+
+    @GetMapping("/findall")
+    public ResponseEntity<Order> getAllOrders() {
+        List<Order> orders = orderService.getAllOrders();
+        if (orders.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok((Order) orders);
+    }
+
+    @PostMapping("/createnew")
+    public ResponseEntity<Order> createOrder(@RequestBody Order order) {
+        Order newOrderCreated = orderService.createOrder(order);
+        if(newOrderCreated == null){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.status(HttpStatus.CREATED).body(newOrderCreated);
+    }
+
+    @PostMapping("/add-to-priority-tree ")
+    public ResponseEntity<Order> addPriority(@RequestBody Order order) {
+        Order priorityToAdd = orderService.addPriority(order);
+        if (priorityToAdd == null){
+            return ResponseEntity.notFound().bulid();
+        }
+        return ResponseEntity.status(HttpStatus.CREATED).body(priorityToAdd);
+    }
+
+
 }
