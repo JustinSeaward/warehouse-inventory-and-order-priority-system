5a14cb8144a8eeec4159918c325b7cd10bf25efa
Justin Seaward <jut_seaward@yahoo.com>
2026-08-14 00:12:09 -0230

    added getAllProducts endpoint method, along with createProduct and the broken getSorted method

--- src/main/java/com/project/product/ProductController.java
+++ src/main/java/com/project/product/ProductController.java
@@ -1,4 +1,38 @@
 package com.project.product;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/product")
 public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping
+    public ResponseEntity<List<Product>> getAllProducts(){
+        List<Product> product = productService.getAllProducts();
+
+        return ResponseEntity.ok(product);
+    }
+
+    @PostMapping
+    public ResponseEntity<Product> createProduct(@RequestBody Product product){
+        Product newProduct = productService.createProduct(product);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newProduct);
+    }
+
+
+    @GetMapping("/sorted/price")
+    public List<Product> getSorted(@RequestParam String by) {
+
+        // TODO: handle multiple sort types
+        return productService.sortByPrice(productService.getAllProducts());
+    }
+
 }
