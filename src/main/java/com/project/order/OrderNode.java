f85d235d2d74f1067434f6b6c00f0b84e91a664c
Justin Seaward <jut_seaward@yahoo.com>
2026-08-13 22:57:19 -0230

    OrderNode: added OrderBST methods to be refactored and have the methods running.

---
+++ src/main/java/com/project/order/OrderNode.java
@@ -0,0 +1,64 @@
+package com.project.order;
+
+public class OrderNode {
+        Order data;
+        OrderNode left;
+        OrderNode right;
+
+        public OrderNode(Order data) {
+            this.data = data;
+        }
+    }
+
+    class OrderBST {
+
+        OrderNode root;
+
+        // TODO: Fix insertion logic
+        public void insert(Order order) {
+            root = insertRecursive(root, order);
+        }
+
+        private OrderNode insertRecursive(OrderNode current, Order order) {
+
+            if (current == null) {
+                return new OrderNode(order);
+            }
+
+            // TODO: Fix the comparisons below. - DONE
+            // Note: priorityLevel values can repeat across orders.
+            // Decide where duplicates should go and be ready to explain your choice.
+
+            // I decide to put the duplicates and lower priority orders to the left side
+            // if they are less then or equal to the parent node, else the
+            // higher priority orders go right.
+            if (order.getPriorityLevel() <= current.data.getPriorityLevel()) {
+                current.left = insertRecursive(current.left, order);
+            } else {
+                current.right = insertRecursive(current.right, order);
+            }
+            return current;
+        }
+
+        // TODO: Implement inorder traversal
+        public void inorder(OrderNode node) {
+            if (node == null) {
+                return;
+            }
+            inorder(node.left);
+            System.out.println(node.data.getPriorityLevel());
+            inorder(node.right);
+        }
+
+
+        // TODO: Fix highest priority logic
+        public Order findHighest() {
+            OrderNode current = root;
+
+            while (current.left != null) {
+                current = current.left;
+            }
+
+            return current.data;
+        }
+    }
